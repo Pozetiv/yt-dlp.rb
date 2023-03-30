5f81aeeede709e82ee91d2aa5d7377dccdea026e
Roman Breslavets <rbreslavets@gmail.com>
2023-03-30 21:17:16 +0300

    chmod -x for binar files

--- lib/yt-dlp/version.rb
+++ lib/yt-dlp/version.rb
@@ -7,5 +7,5 @@
 module YtDlp
   # Semantic Version as well as the bundled binary version.
   # "(major).(minor).(teeny).(pre-release).(binary-version)"
-  VERSION = '0.2.1'
+  VERSION = '0.2.2'
 end
